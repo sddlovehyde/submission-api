7ffdadfa1efe7ab8b36ff763cc308de67d68002c
Wayne <sddlovehyde@hotmail.com>
2020-03-30 15:05:37 +0800

    first commit

---
+++ API_UI.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python2.7
+#  -*- coding: utf-8 -*-
+import subprocess, os, string ,ttk ,time
+import json
+from submission_API import *
+try:
+	from Tkinter import *
+	from tkMessageBox import *
+except ImportError:
+	from tkinter import *
+	
+
+
+class MyFirstGUI:
+	def __init__(self, master):
+        
+                #inital UI windows and config size
+		self.master = master
+		screen_width = root.winfo_screenwidth()
+		screen_height = root.winfo_screenheight()
+		window_size = str(640)+"x"+str(480)+"+0+0"
+		master.title("A simple GUI")
+		master.geometry(window_size)
+		master.title("3PL upload Tool")
+		master.configure(highlightcolor="black")
+
+                #inial info button position and size
+                #define click button event
+             	self.infoButton = Button(master, command=self.getInfo)
+		self.infoButton.place(x=30, y=50, height=30, width=70)
+		self.infoButton.configure(text='''Get Info''')
+		
+                #inial upload button position and size
+                #define click button event
+		self.uploadButton =Button (master, command=self.uploadButton)
+		self.uploadButton.place(x=100, y=50, height=30, width=70)
+		self.uploadButton.configure(text='''upload''')
+        
+                #inial clear button position and size
+                #define clear button event
+		self.clearButton = Button(master, command=self.clear)
+		self.clearButton.place(x=170, y=50, height=30, width=70)
+		self.clearButton.configure(text='''clear''')
+        
+
+        
+                #inial statusbox position, size and font 
+		self.mesListbox = Text(master)
+		self.mesListbox.place(x=20, y=100 , height=300, width=550)
+		self.mesListbox.configure(background="white")
+		self.mesListbox.configure(font="TkFixedFont")
+		self.mesListbox.configure(width=314)
+
+    #unuse function
+	def hit_me():
+		print("12345")
+		print(tk.messagebox.askquestion(title='Hi', message='hahahaha'))
+
+
+	def clear(self):
+
+		self.mesListbox.delete(1.0, END)
+
+	def getInfo(self):
+                fingerprint, clientID = unzip_and_get_info()
+		self.mesListbox.delete(1.0, END)
+		self.mesListbox.insert(END,"FingerPrint:" + fingerprint + "\n")		
+		self.mesListbox.insert(END,"ClientID :"+ clientID + "\n")
+
+
+		
+	def uploadButton(self):		
+		self.mesListbox.insert(END,"-------upload status-------""\n")
+		upload()
+		self.mesListbox.insert(END,"-------upload done-------""\n")
+
+root = Tk()
+my_gui = MyFirstGUI(root)
+root.mainloop()
+
---
+++ submission_API.py
@@ -0,0 +1,275 @@
+import datetime
+import httplib
+import sys
+import json
+import zipfile
+import os
+import shutil
+
+from oauth2client.client import OAuth2WebServerFlow
+from oauth2client.file import Storage
+from oauth2client import tools
+from glob import glob
+
+BASE_URL = 'androidpartner.googleapis.com'
+DEPLOYMENTS_PATH = '/v1/compatibility/{name=devices/*/products/*/builds/*}'
+STORAGE_FILE = 'androidPartner.dat'
+SCOPES = 'https://www.googleapis.com/auth/androidPartner'
+APPLICATION_NAME = 'androidpartner-cmdline-sample/1.7'
+cwd = os.getcwd()
+UPLOAD_TYPE_LIST = ("CTS", "GTS", "CTS_Verify", "CTSonAOSP", "VTS", "STS", "CTSApp")
+
+
+
+def loadJsonFile(path2File):
+  with open(path2File, 'r') as data:
+     jsonData=json.load(data)
+  return jsonData
+
+def findElement(dictionary, key1, key2, element):
+  for i in dictionary:
+    if i[key1] == element:
+       return i[key2]
+
+def unzip(path2File):
+  # a list to save zip file name
+  print path2File
+  zipName = [None] * len(os.listdir(path2File))
+  i = 0
+  j = 0
+  #print os.listdir(path2File)
+  #print zipName
+  # unzip all zip file in folder(path2File) and save .zip file name to list "zipName"
+  for item in os.listdir(path2File):
+    if item.find("zip") != -1:
+      zipName[i] = item
+      #print i
+      #print zipName[i]
+      with zipfile.ZipFile(path2File + '/' + str(zipName[i]), 'r') as zip_ref:
+         zip_ref.extractall(path2File)
+      i = i + 1
+    else:
+      i = i + 1
+      #print i
+  #print zipName
+  # remove none element from list
+  print "unzip file number:", i 
+  zipName = filter(None, zipName)
+  for element in zipName:
+    zipName[j]=element.rstrip(".zip")
+    j = j + 1
+  #print zipName
+  return zipName
+
+
+def uploadReport_1(header):
+  conn = httplib.HTTPSConnection(BASE_URL)
+  path = '/v1/compatibility/report:startUploadReport'
+  headers = header
+  conn.request('POST', path, '', headers)
+  response = json.loads(conn.getresponse().read())
+  print response
+  ref_name = response["ref"]["name"]
+  conn.close()
+  return ref_name
+
+def uploadReport_2(header, ref_name, path2report):
+  conn = httplib.HTTPSConnection(BASE_URL)
+  path = '/upload/v1/media/' +ref_name + '?upload_type=media'
+  headers = header
+  a = open(path2report, mode = 'rb')
+  print path
+  print path2report
+  print "----------upload2-------------"
+  conn.request('POST', path, a, headers)
+  DumpResponseAndCloseConection(conn)
+  print "----------upload2 done-------------"
+
+
+def uploadReport_3(header, ref_name, companyID):
+  conn = httplib.HTTPSConnection(BASE_URL)
+  path = '/v1/compatibility/report'
+  ref_name = str(ref_name)
+  companyID = str(companyID)
+  headers = header
+  body = { "report_ref": {
+               "name": ref_name },               
+               "company_id": companyID
+         }
+  body = str(body)
+  print body
+  print "----------upload3-------------"
+  conn.request('POST', path, body, headers)
+  DumpResponseAndCloseConection(conn)
+  print "----------upload3 done-------------"
+ 
+def GetBuildsList(header):
+  conn = httplib.HTTPSConnection(BASE_URL)
+  path = DEPLOYMENTS_PATH
+  headers = header
+  conn.request('GET', path, '', headers)
+  DumpResponseAndCloseConection(conn)
+
+def GetResponseAndCloseConection(conn):
+  resp = conn.getresponse().read()
+  conn.close()
+
+def DumpResponseAndCloseConection(conn):
+  resp = conn.getresponse().read()
+  print 'the response is:\n' + resp
+  conn.close()
+
+  return resp
+
+def CheckCredentials(client_id, client_secret):
+  # Checks the OAuth credentials before making a request.
+  storage = Storage(STORAGE_FILE)
+  credentials = storage.get()
+  if (not credentials or credentials.invalid or
+      credentials.token_expiry < datetime.datetime.utcnow()):
+    flow = OAuth2WebServerFlow(
+        client_id=client_id,
+        client_secret=client_secret,
+        scope=SCOPES,
+        user_agent=APPLICATION_NAME)
+    credentials = tools.run_flow(flow, storage)
+  return {'Authorization': 'Bearer ' + credentials.access_token}
+
+def unzip_and_get_info():
+  global submissionPack
+  global upload_companyID
+  submissionPackName = unzip(cwd)
+  submissionPack = glob(cwd + "/*/")
+
+  if submissionPackName:
+     print "-----zip mode----"
+     CTS_report = unzip(submissionPack[0] + 'Pega_Report/CTS')
+     GTS_report = unzip(submissionPack[0] + 'Pega_Report/GTS')
+     
+     if (os.path.isfile(submissionPack[0] + 'Pega_Report/CTS/' + CTS_report[0] + '/device-info-files/PropertyDeviceInfo.deviceinfo.json') == True):
+        CTS_report_property = loadJsonFile(submissionPack[0] + 'Pega_Report/CTS/' + CTS_report[0] + '/device-info-files/PropertyDeviceInfo.deviceinfo.json')
+        APA_companyID_dict = loadJsonFile(cwd + '/cusomterID.json')
+        report_clientID = findElement(CTS_report_property["ro_property"], "name", "value", "ro.com.google.clientidbase")
+        report_fingerprint = findElement(CTS_report_property["ro_property"], "name", "value", "ro.build.fingerprint")
+        upload_companyID = findElement(APA_companyID_dict["customer"], "name", "value", report_clientID)
+        
+     elif (os.path.isfile(submissionPack[0] + 'Pega_Report/GTS/' + GTS_report[0] + '/device-info-files/PropertyDeviceInfo.deviceinfo.json') == True):
+        print "CTS no device info, use GTS report to compare"
+        GTS_report_property = loadJsonFile(submissionPack[0] + 'Pega_Report/GTS/' + GTS_report[0] + '/device-info-files/PropertyDeviceInfo.deviceinfo.json')
+        APA_companyID_dict = loadJsonFile(cwd + '/cusomterID.json')
+        report_clientID = findElement(GTS_report_property["ro_property"], "name", "value", "ro.com.google.clientidbase")
+        report_fingerprint = findElement(GTS_report_property["ro_property"], "name", "value", "ro.build.fingerprint")
+        upload_companyID = findElement(APA_companyID_dict["customer"], "name", "value", report_clientID)
+        
+     else:
+        print "no deviceinfo.json file"
+      
+     for item in CTS_report:
+       shutil.rmtree(submissionPack[0] + 'Pega_Report/CTS/' + item)
+
+     for item in GTS_report:
+       shutil.rmtree(submissionPack[0] + 'Pega_Report/GTS/' + item)
+       
+     print "upload company ID:",upload_companyID
+     return report_fingerprint,report_clientID
+
+  elif glob(cwd + "/*/"):
+     print "-----folder mode----"
+     CTS_report = unzip(submissionPack[0] + 'Pega_Report/CTS')
+     GTS_report = unzip(submissionPack[0] + 'Pega_Report/GTS')
+     
+     if (os.path.isfile(submissionPack[0] + 'Pega_Report/CTS/' + CTS_report[0] + '/device-info-files/PropertyDeviceInfo.deviceinfo.json') == True):
+        CTS_report_property = loadJsonFile(submissionPack[0] + 'Pega_Report/CTS/' + CTS_report[0] + '/device-info-files/PropertyDeviceInfo.deviceinfo.json')
+        APA_companyID_dict = loadJsonFile(cwd + '/cusomterID.json')
+        report_clientID = findElement(CTS_report_property["ro_property"], "name", "value", "ro.com.google.clientidbase")
+        report_fingerprint = findElement(CTS_report_property["ro_property"], "name", "value", "ro.build.fingerprint")
+        upload_companyID = findElement(APA_companyID_dict["customer"], "name", "value", report_clientID)
+        
+     elif (os.path.isfile(submissionPack[0] + 'Pega_Report/GTS/' + GTS_report[0] + '/device-info-files/PropertyDeviceInfo.deviceinfo.json') == True):
+        print "CTS no device info, use GTS report to compare"
+        GTS_report_property = loadJsonFile(submissionPack[0] + 'Pega_Report/GTS/' + GTS_report[0] + '/device-info-files/PropertyDeviceInfo.deviceinfo.json')
+        APA_companyID_dict = loadJsonFile(cwd + '/cusomterID.json')
+        report_clientID = findElement(GTS_report_property["ro_property"], "name", "value", "ro.com.google.clientidbase")
+        report_fingerprint = findElement(GTS_report_property["ro_property"], "name", "value", "ro.build.fingerprint")
+        upload_companyID = findElement(APA_companyID_dict["customer"], "name", "value", report_clientID)
+        
+     else:
+        print "no deviceinfo.json file"
+      
+     for item in CTS_report:
+       shutil.rmtree(submissionPack[0] + 'Pega_Report/CTS/' + item)
+
+     for item in GTS_report:
+       shutil.rmtree(submissionPack[0] + 'Pega_Report/GTS/' + item)
+  
+     print "upload company ID:",upload_companyID
+     return report_fingerprint,report_clientID
+
+  else:
+     print "empty"
+     report_fingerprint = "null"
+     report_clientID = "null"
+
+     return report_fingerprint,report_clientID
+     
+
+def upload():
+  header = CheckCredentials('973714329395-r1cjtidkf8jgutb4f2gno2dg9671av81.apps.googleusercontent.com', '_hKnrk2l6ilGgnie9D4n_lJt')
+  ref_name = uploadReport_1(header)
+  for item in UPLOAD_TYPE_LIST:
+    i = 0
+    report_name = os.listdir(submissionPack[0] + 'Pega_Report/' + item)
+    print "report name"
+    print report_name
+    for sub_item in report_name:
+      report_location = submissionPack[0] + 'Pega_Report/' + item + '/' + sub_item
+      print report_location
+      print '******'
+      uploadReport_2(header, ref_name, report_location)
+      uploadReport_3(header, ref_name, upload_companyID)
+      i=i+1
+
+
+
+'''
+
+def main(argv):
+
+  # Developer must pass the project client ID and API key.
+  header = CheckCredentials('973714329395-r1cjtidkf8jgutb4f2gno2dg9671av81.apps.googleusercontent.com', '_hKnrk2l6ilGgnie9D4n_lJt')
+
+  # upzip submission pack & CTS report and get package & CTS folder name
+  submissionPack = unzip(cwd)
+  CTS_report = unzip(cwd + '/' + submissionPack[0] + '/Pega_Report/CTS')
+
+  # load json info and check CTS report client id in customerID.json
+  # get report client ID and upload companyID 
+  CTS_report_property = loadJsonFile(cwd + '/' + submissionPack[0] + '/Pega_Report/CTS/' + CTS_report[0] + '/device-info-files/PropertyDeviceInfo.deviceinfo.json')
+  APA_companyID_dict = loadJsonFile(cwd + '/cusomterID.json')
+  report_clientID = findElement(CTS_report_property["ro_property"], "name", "value", "ro.com.google.clientidbase")  
+  upload_companyID = findElement(APA_companyID_dict["customer"], "name", "value", report_clientID)
+  print upload_companyID
+
+  # remove CTS folder
+  for item in CTS_report:
+    shutil.rmtree(cwd + '/' + submissionPack[0] + '/Pega_Report/CTS/' + item)
+  ref_name = uploadReport_1(header)
+
+  # upload report
+  for item in UPLOAD_TYPE_LIST:
+    i = 0
+    report_name = os.listdir(cwd + '/' + submissionPack[0] + '/Pega_Report/' + item)
+    for sub_item in report_name:
+      report_location = cwd + '/' + submissionPack[0] + '/Pega_Report/' + item + '/' + sub_item
+      print report_location
+      print '******'
+      #uploadReport_2(header, ref_name, report_location)
+      #uploadReport_3(header, ref_name, upload_companyID)
+      i=i+1
+
+
+
+
+if __name__ == '__main__':
+  main(sys.argv)
+'''
